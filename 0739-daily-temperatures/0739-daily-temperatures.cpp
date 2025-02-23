890fa8f7741e62752a19e4581554be29e026a991
Shubham Kumar <80589732+Shubham-k@users.noreply.github.com>
2025-02-23 22:57:08 +0530

    Time: 36 ms (17.83%), Space: 108.1 MB (21.22%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        // 73,74,75,71,69,72,76,73
+        // 74 75 76 72 72 76 -1 -1
+
+        // 73,76,72,69,71,75,74,73
+        // -1 -1 76 72 72 76 75 74
+
+        // 74 75 76 72 72 76 -1 -1
+
+        // =======================
+
+        // 73,74,75,71,69,72,76,73
+        // 1  1  4  2  1  1  0  0
+
+        // calculate -> 0  0  1  1  2  4  1  1
+        // elements -> 73,76,72,69,71,75,74,73
+        // index ->    0  1  2  3  5  6  7  8
+
+        // 1 1 4 2 1 1 0 0
+
+        reverse(temperatures.begin(), temperatures.end());
+        stack<pair<int, int>> s;
+        vector<int> ans;
+
+        int index = 0;
+        s.push({temperatures[0], index});
+        ans.push_back(0);
+
+        for (int i = 1; i < temperatures.size(); i++) {
+            index++;
+
+            while (!s.empty() && s.top().first <= temperatures[i]) {
+                s.pop();
+            }
+
+            int res = s.empty() ? 0 : index - s.top().second;
+            ans.push_back(res);
+            s.push({temperatures[i], index});
+        }
+
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
